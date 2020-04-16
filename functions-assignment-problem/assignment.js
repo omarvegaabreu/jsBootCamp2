be726fe5227fa0748cf6d749603ee5a963ccf176
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-04-16 19:17:58 -0400

    did assigment 2 on functions

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,31 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = (name) => {
+  console.log("Hi " + name);
+};
+sayHello("Omar");
 
-sayHello();
+const sayHelloAgain = (name, greeting) => {
+  console.log(`THIS IS WITH 2 ARGUMENTS: ${greeting},${name}`);
+};
+sayHelloAgain("Omar", "Hi");
+
+const sayHelloAgainNoArgs = () => {
+  console.log(`Hi Omar with no arguments`);
+};
+sayHelloAgainNoArgs();
+
+const sayHelloReturn = (name = "JoseJose") => {
+  return `"Hi  ${name}`;
+};
+
+console.log(sayHelloReturn());
+
+const greeting = (name) => {
+  alert("Hello " + name);
+};
+
+const processUserInput = (callback) => {
+  const name = prompt("Please enter your name.");
+  callback(name);
+};
+
+processUserInput(greeting);
