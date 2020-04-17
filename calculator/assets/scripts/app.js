6052bc99895dc6f524453869d5edfa922f8d1faf
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-04-17 16:31:54 -0400

    added boiler plate

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,45 +1,65 @@
-const defaultValue = 0;
-let currentResult = defaultValue;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-function getUSerInput() {
-  return parseInt(userInput.value);
-}
+// Gets input from input field
+const getUserNumberInput = () => {
+  return parseInt(usrInput.value);
+};
 
-function createAndWriteOutPut(operator, resultBeforeCal, calNumber) {
-  return ` ${resultBeforeCal} ${operator} ${calNumber}`;
-}
+// Generates and writes calculation log
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
+};
 
-function add() {
-  const inputValue = getUSerInput();
-  // const calDescription = ` ${currentResult} + ${inputValue}`;
-  currentResult = currentResult + inputValue;
-  createAndWriteOutPut("+", currentResult, calDescription);
-}
+//push to log function
+const writeToLog = (
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) => {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+};
+//programming logic for calculations
+const calculate = (operation) => {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  switch (operation) {
+    case operation === "ADD":
+      currentResult += enteredNumber;
+      createAndWriteOutput("+", initialResult, enteredNumber);
+      writeToLog("ADD", initialResult, enteredNumber, currentResult);
+      break;
+    case operation === "SUBTRACT":
+      currentResult -= enteredNumber;
+      createAndWriteOutput("-", initialResult, enteredNumber);
+      writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+      break;
+    case operation === "MULTIPLICATION":
+      currentResult *= enteredNumber;
+      createAndWriteOutput("*", initialResult, enteredNumber);
+      writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+      break;
+    case operation === "DIVISION":
+      currentResult /= enteredNumber;
+      createAndWriteOutput("/", initialResult, enteredNumber);
+      writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+      break;
+    default:
+      break;
+  }
+};
 
-function subtract() {
-  currentResult = defaultValue;
-  const inputValue = getUSerInput();
-  // const calDescription = ` ${currentResult} - ${inputValue}`;
-  currentResult = currentResult - inputValue;
-  createAndWriteOutPut("-", currentResult, calDescription);
-}
-
-function multiply() {
-  currentResult = defaultValue;
-  const inputValue = getUSerInput();
-  // const calDescription = ` ${currentResult} - ${inputValue}`;
-  currentResult = currentResult * inputValue;
-  createAndWriteOutPut("*", currentResult, calDescription);
-}
-
-function divide() {
-  currentResult = defaultValue;
-  const inputValue = getUSerInput();
-  // const calDescription = ` ${currentResult} - ${inputValue}`;
-  currentResult = currentResult / inputValue;
-  createAndWriteOutPut("/", currentResult, calDescription);
-}
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculate.bind(this, "ADD"));
+subtractBtn.addEventListener("click", calculate.bind(this, "SUBTRACT"));
+multiplyBtn.addEventListener("click", calculate.bind(this, "MULTIPLY"));
+divideBtn.addEventListener("click", calculate.bind(this, "DIVIDE"));
--- functions-10-callback-functions/app.js
+++ functions-10-callback-functions/app.js
@@ -1,19 +1,19 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-const ROCK = 'ROCK';
-const PAPER = 'PAPER';
-const SCISSORS = 'SCISSORS';
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
 const DEFAULT_USER_CHOICE = ROCK;
-const RESULT_DRAW = 'DRAW';
-const RESULT_PLAYER_WINS = 'PLAYER_WINS';
-const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
 
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
   const selection = prompt(
     `${ROCK}, ${PAPER} or ${SCISSORS}?`,
-    ''
+    ""
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
@@ -42,24 +42,12 @@ const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
     ? RESULT_PLAYER_WINS
     : RESULT_COMPUTER_WINS;
 
-// if (cChoice === pChoice) {
-//   return RESULT_DRAW;
-// } else if (
-//   (cChoice === ROCK && pChoice === PAPER) ||
-//   (cChoice === PAPER && pChoice === SCISSORS) ||
-//   (cChoice === SCISSORS && pChoice === ROCK)
-// ) {
-//   return RESULT_PLAYER_WINS;
-// } else {
-//   return RESULT_COMPUTER_WINS;
-// }
-
-startGameBtn.addEventListener('click', () => {
+startGameBtn.addEventListener("click", () => {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
-  console.log('Game is starting...');
+  console.log("Game is starting...");
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   let winner;
@@ -68,14 +56,15 @@ startGameBtn.addEventListener('click', () => {
   } else {
     winner = getWinner(computerChoice);
   }
-  let message = `You picked ${playerChoice ||
-    DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+  let message = `You picked ${
+    playerChoice || DEFAULT_USER_CHOICE
+  }, computer picked ${computerChoice}, therefore you `;
   if (winner === RESULT_DRAW) {
-    message = message + 'had a draw.';
+    message = message + "had a draw.";
   } else if (winner === RESULT_PLAYER_WINS) {
-    message = message + 'won.';
+    message = message + "won.";
   } else {
-    message = message + 'lost.';
+    message = message + "lost.";
   }
   alert(message);
   gameIsRunning = false;
@@ -95,18 +84,19 @@ const sumUp = (resultHandler, ...numbers) => {
   resultHandler(sum);
 };
 
-const subtractUp = function() {
+const subtractUp = function (resultHandler, ...numbers) {
   let sum = 0;
-  for (const num of arguments) { // don't use that
+  for (const num of numbers) {
+    // don't use that
     sum -= num;
   }
-  return sum;
+  resultHandler(sum);
 };
 
 const showResult = (result) => {
-  alert('The result after adding all numbers is: ' + result);
+  alert("The result after adding all numbers is: " + result);
 };
 
-sumUp(showResult, 1, 5, 'fdsa', -3, 6, 10);
+sumUp(showResult, 1, 5, "fdsa", -3, 6, 10);
 sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
 console.log(subtractUp(1, 10, 15, 20));
