4751ef215779085e02e757922bf8ae266c986191
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-04-18 07:57:52 -0400

    assigment solved

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,7 @@
+const li = document.getElementById("task-1");
+li.style.backgroundColor = "black";
+li.style.color = "white";
+const head = document.querySelector("head");
+head.textContent = "this is new head";
+const h1 = document.querySelector("h1");
+h1.textContent = "assigment solved";
