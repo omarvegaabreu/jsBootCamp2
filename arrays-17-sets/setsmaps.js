5a9d008eb22a786b273e74774d6750e09824af40
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-04-23 11:38:03 -0400

    added boiler plate

--- arrays-17-sets/setsmaps.js
+++ arrays-17-sets/setsmaps.js
@@ -1,9 +1,7 @@
-// const ids = new Set(['Hi', 'from', 'set!']);
-// ids.add(2);
-// if (ids.has('Hi')) {
-//   ids.delete('Hi');
-// }
+// const id = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// id.add(11);
+// console.log(id);
 
-// for (const entry of ids.entries()) {
-//   console.log(entry[0]);
+// for (const entry of id.entries()) {
+//   console.log(entry);
 // }
