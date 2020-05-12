c24827fed51cef4247ee642457a8d39cf655ed91
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-05-12 15:04:30 -0400

    promises error handling

--- async-02-sync-code-loop/app.js
+++ async-02-sync-code-loop/app.js
@@ -1,15 +1,41 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolved, reject) => {
+    setTimeout(() => {
+      resolved("timer done");
+    }, duration);
+  });
+  return promise;
+};
+
+const getPosition = (options) => {
+  const promise = new Promise((resolved, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolved(success);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    (positionData) => {
+  getPosition()
+    .then((positionData) => {
       console.log(positionData);
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+    })
+    .catch((error) => {
+      console.log("this is my error" + error);
+    });
+
+  // setTimer(2000).then((done) => {
+  //   console.log(done);
+  // });
 }
 
 button.addEventListener("click", trackUserHandler);
